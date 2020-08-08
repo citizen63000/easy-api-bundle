f27a9074246b8d66f3022a402c09a5d04291b46a
lionel <ngconnected@gmail.com>
2020-08-08 13:00:47 +0200

    new CrudDescribeFormControllerTrait

---
+++ Util/Controller/CrudDescribeFormControllerTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EasyApiBundle\Util\Controller;
+
+trait CrudDescribeFormControllerTrait
+{
+    /**
+     * Describe fields of form.
+     *
+     * @Swagger\Annotations\Response(
+     *     response=200,
+     *     description="Successful operation",
+     *     @Swagger\Annotations\Schema(ref=@Nelmio\ApiDocBundle\Annotation\Model(type="EasyApiBundle\Util\Forms\SerializedForm", groups={"public"}))
+     * ),
+     *
+     * @Swagger\Annotations\Response(response="404", ref="#/definitions/404"),
+     * @Swagger\Annotations\Response(response="405", ref="#/definitions/405"),
+     * @Swagger\Annotations\Response(response="415", ref="#/definitions/415")
+     *
+     * @FOS\RestBundle\Controller\Annotations\View(serializerGroups={ "public" })
+     *
+     * @return EasyApiBundle\Util\Forms\SerializedForm
+     */
+    public function describeFormAction()
+    {
+        return $this->describeForm(static::$entityTypeClass);
+    }
+}
