3b1f675903751842e27d16d877ebaf762306ba28
citizen63000 <66557423+citizen63000@users.noreply.github.com>
2020-08-17 12:50:56 +0200

    Update CrudDescribeFormControllerTrait.php

--- Util/Controller/CrudDescribeFormControllerTrait.php
+++ Util/Controller/CrudDescribeFormControllerTrait.php
@@ -2,6 +2,9 @@
 
 namespace EasyApiBundle\Util\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use EasyApiBundle\Util\Forms\SerializedForm;
+
 trait CrudDescribeFormControllerTrait
 {
     /**
@@ -13,18 +16,24 @@ trait CrudDescribeFormControllerTrait
      *     response=200,
      *     description="Successful operation",
      *     @Swagger\Annotations\Schema(ref=@Nelmio\ApiDocBundle\Annotation\Model(type="EasyApiBundle\Util\Forms\SerializedForm", groups={"public"}))
-     * ),
+     * )
      *
-     * @Swagger\Annotations\Response(response="404", ref="#/definitions/404"),
-     * @Swagger\Annotations\Response(response="405", ref="#/definitions/405"),
+     * @Swagger\Annotations\Response(response="404", ref="#/definitions/404")
+     * @Swagger\Annotations\Response(response="405", ref="#/definitions/405")
      * @Swagger\Annotations\Response(response="415", ref="#/definitions/415")
      *
      * @FOS\RestBundle\Controller\Annotations\View(serializerGroups={ "public" })
      *
-     * @return EasyApiBundle\Util\Forms\SerializedForm
+     * @param Request $request
+     *
+     * @return SerializedForm
      */
-    public function describeFormAction()
+    public function describeFormAction(Request $request): SerializedForm
     {
-        return $this->describeForm(static::entityTypeClass);
+        $method = strtoupper($request->query->get('method', 'POST'));
+
+        $form = 'POST' === $method ? static::entityCreateTypeClass : static::entityUpdateTypeClass;
+
+        return $this->describeForm($form);
     }
 }
