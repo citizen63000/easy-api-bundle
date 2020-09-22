97af3adc8b05dfd9fb18ac6dca23ab47fe6430bd
citizen63000 <citizen.lantien@gmail.com>
2020-09-22 21:18:35 +0200

    UpdateTestFunctionsTrait

--- Util/Tests/crud/UpdateTestFunctionsTrait.php
+++ Util/Tests/crud/UpdateTestFunctionsTrait.php
@@ -6,26 +6,26 @@ use EasyApiBundle\Util\ApiProblem;
 use EasyApiBundle\Util\Tests\Format;
 use Symfony\Component\HttpFoundation\Response;
 
-trait CreateTestFunctionsTrait
+trait UpdateTestFunctionsTrait
 {
     use crudFunctionsTestTrait;
 
     /**
-     * POST - Nominal case.
+     * PUT - Nominal case.
      * @param string|null $filename
      * @param array $params
      */
-    protected function doTestCreate(string $filename = null, $params = []): void
+    protected function doTestUpdate(string $filename = null, $params = []): void
     {
         // Request
 
         if (null !== $filename) {
-            $data = $this->getDataSent($filename, 'Create');
+            $data = $this->getDataSent($filename, 'Update');
         } else {
-            $data = self::createPOSTData();
+            $data = self::createPUTData();
         }
 
-        $apiOutput = self::httpPost(['name' => static::getCreateRouteName(), 'params' => $params], $data);
+        $apiOutput = self::httpPut(['name' => static::getUpdateRouteName(), 'params' => $params], $data);
 
         static::assertEquals(Response::HTTP_CREATED, $apiOutput->getStatusCode());
 
@@ -33,44 +33,44 @@ trait CreateTestFunctionsTrait
 
         $result = static::assertCreatedUpdatedAt($apiOutput->getData());
         if (null !== $filename) {
-            $expectedResult = $this->getExpectedResponse($filename, 'Create', $apiOutput->getData());
+            $expectedResult = $this->getExpectedResponse($filename, 'Update', $apiOutput->getData());
         } else {
-            $expectedResult = self::createPOSTResponseData();
+            $expectedResult = self::createPUTResponseData();
         }
 
         static::assertEquals($expectedResult, $result);
 
-        // GET AFTER POST
+        // GET AFTER PUT
         $apiOutput = self::httpGet(['name' => static::getGetRouteName(), 'params' => ['id' => $expectedResult['id']]]);
         static::assertEquals(Response::HTTP_OK, $apiOutput->getStatusCode());
         static::assertEquals($expectedResult, $apiOutput->getData());
     }
 
     /**
-     * POST - Error case - 401 - Without authentication.
+     * PUT - Error case - 401 - Without authentication.
      * @param string|null $filename
      * @param array $params
      */
-    protected function doTestCreateWithoutAuthentication(string $filename = null, $params = []): void
+    protected function doTestUpdateWithoutAuthentication(string $filename = null, $params = []): void
     {
         if (null !== $filename) {
-            $data = $this->getDataSent($filename, 'Create');
+            $data = $this->getDataSent($filename, 'Update');
         } else {
-            $data = self::createPOSTData();
+            $data = self::getUpdateRouteName();
         }
 
-        $apiOutput = self::httpPost(['name' => static::getCreateRouteName(), 'params' => $params], $data, false);
+        $apiOutput = self::httpPut(['name' => static::getUpdateRouteName(), 'params' => $params], $data, false);
         static::assertApiProblemError($apiOutput, Response::HTTP_UNAUTHORIZED, [ApiProblem::JWT_NOT_FOUND]);
     }
 
     /**
-     * POST - Error case - 403 - Missing right.
+     * PUT - Error case - 403 - Missing right.
      * @param string|null $filename
      * @param array $params
      * @param string|null $userLogin
      * @param string|null $userPassword
      */
-    protected function doTestCreateWithoutRight(string $filename = null, array $params = [], string $userLogin = null, string $userPassword = null): void
+    protected function doTestUpdateWithoutRight(string $filename = null, array $params = [], string $userLogin = null, string $userPassword = null): void
     {
         if (null === $userPassword && null !== $userLogin) {
             throwException(new \Exception('$userPassword parameter cannot be null if $userLogin parameters is not null'));
@@ -84,13 +84,13 @@ trait CreateTestFunctionsTrait
         $token = self::loginHttp($userLogin, $userPassword);
 
         if (null !== $filename) {
-            $data = $this->getDataSent($filename, 'Create');
+            $data = $this->getDataSent($filename, 'Update');
         } else {
-            $data = self::createPOSTData();
+            $data = self::createPUTData();
         }
 
-        $apiOutput = self::httpPost([
-            'name' => static::getCreateRouteName(),
+        $apiOutput = self::httpPut([
+            'name' => static::getUpdateRouteName(),
             'params' => $params,
         ], $data, false, Format::JSON, Format::JSON, ['Authorization' => self::getAuthorizationTokenPrefix() . " {$token}"]);
 
