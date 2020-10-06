e8bf69dc53f909b61cf0e04d832775ecda3444d2
lionel <l.antraigue@fidroit.fr>
2020-10-06 13:37:39 +0200

    DeleteTestTrait.php

---
+++ Util/Tests/crud/DeleteTestTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EasyApiBundle\Util\Tests\crud;
+
+trait DeleteTestTrait
+{
+    use DeleteTestFunctionsTrait;
+
+    /**
+     * DELETE - Nominal case.
+     */
+    public function testDelete(): void
+    {
+        $this->doTestDelete();
+    }
+
+    /**
+     * DELETE - Unexisting entity.
+     */
+    public function testDeleteNotFound(): void
+    {
+        $this->doTestDeleteNotFound(9999999);
+    }
+
+    /**
+     * DELETE - Error case - 401 - Without authentication.
+     */
+    public function testDeleteWithoutAuthentication(): void
+    {
+        $this->doTestDeleteWithoutAuthentication();
+    }
+
+    /**
+     * DELETE - Error case - 403 - Missing right.
+     */
+    public function testDeleteWithoutRight403(): void
+    {
+        $this->doTestDeleteWithoutRight();
+    }
+}
