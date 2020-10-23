414d53122ac4d1f861f191a1ea0e15420ac4c6d9
lionel <ngconnected@gmail.com>
2020-10-23 17:20:37 +0200

    tests avec login possible sur chaque test

--- Util/Tests/crud/functions/GetTestFunctionsTrait.php
+++ Util/Tests/crud/functions/GetTestFunctionsTrait.php
@@ -3,8 +3,6 @@
 namespace EasyApiBundle\Util\Tests\crud\functions;
 
 use EasyApiBundle\Util\ApiProblem;
-use EasyApiBundle\Util\Tests\Format;
-use ReflectionException;
 use Symfony\Component\HttpFoundation\Response;
 
 trait GetTestFunctionsTrait
@@ -15,21 +13,16 @@ trait GetTestFunctionsTrait
      * GET - Nominal case.
      * @param int|null $id
      * @param string|null $filename
+     * @param string|null $userLogin
+     * @param string|null $userPassword
      */
-    public function doTestGet(int $id = null, string $filename = null): void
+    public function doTestGet(int $id = null, string $filename = 'nominalCase.json', string $userLogin = null, string $userPassword = null): void
     {
-        $id = $id ?? static::defaultEntityId;
-
-        $apiOutput = self::httpGet(['name' => static::getGetRouteName(), 'params' => ['id' => $id]]);
+        $apiOutput = self::httpGetWithLogin(['name' => static::getGetRouteName(), 'params' => ['id' => $id ?? static::defaultEntityId]], $userLogin, $userPassword);
 
         self::assertEquals(Response::HTTP_OK, $apiOutput->getStatusCode());
         $result = $apiOutput->getData();
-
-        if(null !== $filename) {
-            $expectedResult = $this->getExpectedResponse($filename, 'Get', $result);
-        } else {
-            $expectedResult = self::createGETResponseData();
-        }
+        $expectedResult = $this->getExpectedResponse($filename, 'Get', $result);
 
         static::assertAssessableContent($expectedResult, $result);
         static::assertEquals($expectedResult, $result);
@@ -37,17 +30,19 @@ trait GetTestFunctionsTrait
 
     /**
      * GET - Error case - not found.
-     * @param int $id
+     * @param int|null $id
+     * @param string|null $userLogin
+     * @param string|null $userPassword
      */
-    public function doTestGetNotFound(int $id = null): void
+    public function doTestGetNotFound(int $id = null, string $userLogin = null, string $userPassword = null): void
     {
-        $apiOutput = self::httpGet(['name' => static::getGetRouteName(), 'params' => ['id' => $id ?? 99999999]]);
-        static::assertApiProblemError($apiOutput, Response::HTTP_NOT_FOUND, [sprintf(ApiProblem::ENTITY_NOT_FOUND, 'entity')]);
+        $apiOutput = self::httpGetWithLogin(['name' => static::getGetRouteName(), 'params' => ['id' => $id ?? 99999999]]);
+        static::assertApiProblemError($apiOutput, Response::HTTP_NOT_FOUND, [sprintf(ApiProblem::ENTITY_NOT_FOUND, 'entity')], $userLogin, $userPassword);
     }
 
     /**
      * GET - Error case - 401 - Without authentication.
-     * @param int $id
+     * @param int|null $id
      */
     public function doTestGetWithoutAuthentication(int $id = null): void
     {
@@ -63,22 +58,12 @@ trait GetTestFunctionsTrait
      */
     public function doTestGetWithoutRight(int $id = null, string $userLogin = null, string $userPassword = null): void
     {
-        if(null === $userPassword && null!== $userLogin) {
-            throwException(new \Exception('$userPassword parameter cannot be null if $userLogin parameters is not null'));
-        }
-
         if(null === $userLogin && null === $userPassword) {
             $userLogin = static::USER_NORULES_TEST_USERNAME;
             $userPassword = static::USER_NORULES_TEST_PASSWORD;
         }
 
-        $token = self::loginHttp($userLogin, $userPassword);
-        $apiOutput = self::httpGet([
-            'name' => static::getGetRouteName(), 'params' => ['id' => $id ?? static::defaultEntityId]],
-            false,
-            Format::JSON,
-            ['Authorization' => self::getAuthorizationTokenPrefix()." {$token}"]
-        );
+        $apiOutput = self::httpGetWithLogin(['name' => static::getGetRouteName(), 'params' => ['id' => $id ?? static::defaultEntityId]], $userLogin, $userPassword);
 
         static::assertApiProblemError($apiOutput, Response::HTTP_FORBIDDEN, [ApiProblem::RESTRICTED_ACCESS]);
     }
