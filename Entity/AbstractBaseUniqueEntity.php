a69597eb0b2ab082199cbb664f30c9063642687f
lionel <ngconnected@gmail.com>
2021-05-31 23:52:18 +0200

    AbstractBaseUniqueEntity.php & MagicGettersSettersTrait.php

---
+++ Entity/AbstractBaseUniqueEntity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EasyApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+abstract class AbstractBaseUniqueEntity extends AbstractBaseEntity
+{
+    /**
+     * @var UuidInterface
+     * @ORM\Column(type="uuid")
+     */
+    protected $uuid;
+
+    /**
+     * AbstractBaseUniqueEntity constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->uuid = Uuid::uuid4();
+    }
+
+    /**
+     * @return UuidInterface|null
+     */
+    public function getUuid(): ?UuidInterface
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @param UuidInterface|null $uuid
+     */
+    public function setUuid(?UuidInterface $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+}
---
+++ Entity/MagicGettersSettersTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EasyApiBundle\Entity;
+
+trait MagicGettersSettersTrait
+{
+    public function __get($property)
+    {
+        return $this->property;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->property= $value;
+    }
+}
