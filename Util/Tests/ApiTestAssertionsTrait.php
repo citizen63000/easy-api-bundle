e8159c738cc6a5b1dd6cb56c556d623548bba0ce
lionel <ngconnected@gmail.com>
2021-06-01 18:38:23 +0200

    tests : uri_prefix option in  assertFileUrl

--- Util/Tests/ApiTestAssertionsTrait.php
+++ Util/Tests/ApiTestAssertionsTrait.php
@@ -220,6 +220,7 @@ trait ApiTestAssertionsTrait
         $expectedUUID = '[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+';
         $expected = str_replace('{UUID}', $expectedUUID, $expected);
         $expected = str_replace('{UID}', '[a-zA-Z0-9]+', $expected);
+        $expected = str_replace('{uri_prefix}', static::getDomainUrl(), $expected);
         $expected = "/$expected/";
         $errorMessage = "Invalid file url in {$key} field: expected {$expected}, get value {$value}";
         $found = preg_match($expected, $value);
