0e542b5688cd8c3a1944fb9846918062e3e6da8c
lionel <l.antraigue@fidroit.fr>
2021-09-02 21:07:06 +0200

    Improve PrivateMediaSerializer.php

--- Serializer/PrivateMediaSerializer.php
+++ Serializer/PrivateMediaSerializer.php
@@ -4,8 +4,8 @@ namespace EasyApiBundle\Serializer;
 
 use EasyApiBundle\Entity\MediaUploader\AbstractPrivateMedia;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Routing\Router;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 
@@ -13,22 +13,19 @@ class PrivateMediaSerializer extends AbstractObjectSerializer
 {
     private RouterInterface $router;
 
-    private string $routerContextScheme;
-    private string $routerContextHost;
+    private ParameterBagInterface $parameterBag;
 
     /**
      * SerializationListenerFeature constructor.
      * @param ContainerInterface $container
      * @param RouterInterface $router
-     * @param string $routerContextScheme
-     * @param string $routerContextHost
+     * @param ParameterBagInterface $parameterBag
      */
-    public function __construct(ContainerInterface $container, RouterInterface $router, string $routerContextScheme, string $routerContextHost)
+    public function __construct(ContainerInterface $container, RouterInterface $router, ParameterBagInterface $parameterBag)
     {
         parent::__construct($container);
         $this->router = $router;
-        $this->routerContextScheme = $routerContextScheme;
-        $this->routerContextHost = $routerContextHost;
+        $this->parameterBag = $parameterBag;
     }
 
     /**
@@ -43,7 +40,7 @@ class PrivateMediaSerializer extends AbstractObjectSerializer
         $data = parent::normalize($object, $format, $context);
 
         if ($object->getFilename() && !empty($route = $object::getDownloadRouteName())) {
-            $domain = "{$this->routerContextScheme}://{$this->routerContextHost}";
+            $domain = "{$this->parameterBag->get('router.request_context.scheme')}://{$this->parameterBag->get('router.request_context.host')}";
             $data['fileUrl'] = $domain.$this->router->generate($route, ['id' => $object->getId()], UrlGeneratorInterface::ABSOLUTE_PATH);
         }
 
