8643fcd4ab47feb2a113a06d0ddea5d58c469924
lionel <l.antraigue@fidroit.fr>
2021-09-23 08:07:57 +0200

    return apioutput on getListTest functions

--- Util/Tests/crud/functions/GetListTestFunctionsTrait.php
+++ Util/Tests/crud/functions/GetListTestFunctionsTrait.php
@@ -3,6 +3,7 @@
 namespace EasyApiBundle\Util\Tests\crud\functions;
 
 use EasyApiBundle\Util\ApiProblem;
+use EasyApiBundle\Util\Tests\ApiOutput;
 use Symfony\Component\HttpFoundation\Response;
 
 trait GetListTestFunctionsTrait
@@ -15,8 +16,9 @@ trait GetListTestFunctionsTrait
      * @param array|null $params
      * @param string|null $userLogin
      * @param string|null $userPassword
+     * @return ApiOutput
      */
-    protected function doTestGetList(string $filename, array $params = [], string $userLogin = null, string $userPassword = null): void
+    protected function doTestGetList(string $filename, array $params = [], string $userLogin = null, string $userPassword = null): ApiOutput
     {
         $apiOutput = self::httpGetWithLogin(['name' => static::getGetListRouteName(), 'params' => $params], $userLogin, $userPassword);
 
@@ -25,6 +27,8 @@ trait GetListTestFunctionsTrait
         $expectedResult = $this->getExpectedResponse($filename, self::$getListActionType, $apiOutput->getData());
 
         static::assertEquals($expectedResult, $apiOutput->getData(), "Assert failed for file {$filename}");
+
+        return $apiOutput;
     }
 
     /**
@@ -32,8 +36,11 @@ trait GetListTestFunctionsTrait
      * @param int|null $page
      * @param int|null $limit
      * @param array $params
+     * @param string|null $userLogin
+     * @param string|null $userPassword
+     * @return ApiOutput
      */
-    protected function doTestGetListPaginate(string $filename, int $page = null, int $limit = null, array $params = [], string $userLogin = null, string $userPassword = null)
+    protected function doTestGetListPaginate(string $filename, int $page = null, int $limit = null, array $params = [], string $userLogin = null, string $userPassword = null): ApiOutput
     {
         try {
             $pagination = [];
@@ -43,7 +50,7 @@ trait GetListTestFunctionsTrait
             if (null !== $limit) {
                 $pagination['limit'] = $limit;
             }
-            $this->doTestGetList($filename, $pagination + $params, $userLogin, $userPassword);
+            return $this->doTestGetList($filename, $pagination + $params, $userLogin, $userPassword);
         } catch (ReflectionException $e) {
             echo $e->getMessage();
         }
@@ -58,27 +65,32 @@ trait GetListTestFunctionsTrait
      * @param array $params
      * @param string|null $userLogin
      * @param string|null $userPassword
+     * @return ApiOutput
      */
-    protected function doTestGetListFiltered(string $filename, int $page = null, int $limit = null, array $filters = [], string $sort = null, array $params = [], string $userLogin = null, string $userPassword = null)
+    protected function doTestGetListFiltered(string $filename, int $page = null, int $limit = null, array $filters = [], string $sort = null, array $params = [], string $userLogin = null, string $userPassword = null): ApiOutput
     {
-        $this->doTestGetListPaginate($filename, $page, $limit, $filters + ['sort' => $sort] + $params, $userLogin, $userPassword);
+        return $this->doTestGetListPaginate($filename, $page, $limit, $filters + ['sort' => $sort] + $params, $userLogin, $userPassword);
     }
 
     /**
      * GET - Error case - 401 - Without authentication.
+     * @return ApiOutput
      */
-    protected function doTestGetWithoutAuthentication(): void
+    protected function doTestGetWithoutAuthentication(): ApiOutput
     {
         $apiOutput = self::httpGet(['name' => static::getGetListRouteName(), 'params' => []], false);
         static::assertApiProblemError($apiOutput, Response::HTTP_UNAUTHORIZED, [ApiProblem::JWT_NOT_FOUND]);
+
+        return $apiOutput;
     }
 
     /**
      * GET - Error case - 403 - Missing right.
      * @param string|null $userLogin
      * @param string|null $userPassword
+     * @return ApiOutput
      */
-    protected function doTestGetWithoutRight(string $userLogin = null, string $userPassword = null): void
+    protected function doTestGetWithoutRight(string $userLogin = null, string $userPassword = null): ApiOutput
     {
         if (null === $userLogin && null === $userPassword) {
             $userLogin = static::USER_NORULES_TEST_USERNAME;
@@ -88,5 +100,7 @@ trait GetListTestFunctionsTrait
         $apiOutput = self::httpGetWithLogin(['name' => static::getGetListRouteName(), 'params' => []], $userLogin, $userPassword);
 
         static::assertApiProblemError($apiOutput, Response::HTTP_FORBIDDEN, [ApiProblem::RESTRICTED_ACCESS]);
+
+        return $apiOutput;
     }
 }
