4a3b47b23313e84eeb83ad17811052b768c8eee1
lionel <ngconnected@gmail.com>
2022-05-12 13:29:24 +0200

    improve PascalCase conversions

--- Util/StringUtils/CaseConverter.php
+++ Util/StringUtils/CaseConverter.php
@@ -13,7 +13,27 @@ class CaseConverter
      */
     public static function convertSnakeCaseToPascalCase(string $str): string
     {
-        $words = explode('_', $str);
+        return static::convertDelimitedTextToPascalCase($str, '_');
+    }
+
+    /**
+     * @example my-variable-name => MyVariableName
+     * @param string $str
+     * @return string
+     */
+    public static function convertSpinalCaseToPascalCase(string $str): string
+    {
+        return static::convertDelimitedTextToPascalCase($str, '-');
+    }
+
+    /**
+     * @param string $str
+     * @param string $delimiter
+     * @return string
+     */
+    public static function convertDelimitedTextToPascalCase(string $str, string $delimiter): string
+    {
+        $words = explode($delimiter, $str);
 
         foreach ($words as $key => $word) {
             $words[$key] = ucfirst($word);
@@ -21,6 +41,7 @@ class CaseConverter
 
         return implode('', $words);
     }
+
 
     /**
      * @example my_variable_name => myVariableName
@@ -56,13 +77,13 @@ class CaseConverter
     }
 
     /**
-     * @example MyVariableName
+     * @example my_variable_name
      *
      * @param string $str
      *
      * @return string
      */
-    public static function convertToPascalCase(string $str)
+    public static function convertToSnakeCase(string $str)
     {
         return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $str));
     }
