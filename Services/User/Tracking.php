44dbc7a58403c663d257a85cdbc6b859b6f6793f
lionel <ngconnected@gmail.com>
2022-08-04 13:40:32 +0200

    flex APIs : remove old user agent parser

--- Services/User/Tracking.php
+++ Services/User/Tracking.php
@@ -8,22 +8,11 @@ use EasyApiBundle\Services\AbstractService;
 use Namshi\JOSE\JWS;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
-use UserAgentParser\Exception\NoResultFoundException;
-use UserAgentParser\Model\UserAgent;
-use UserAgentParser\Provider\WhichBrowser;
 
 class Tracking extends AbstractService
 {
     const CONNECTION_HISTORY_CLASS_PARAMETER = 'easy_api.user_tracking.connection_history_class';
     const TRACKING_ENABLE_PARAMETER = 'easy_api.user_tracking.enable';
-
-    /**
-     * @return array
-     */
-    protected function getProviders(): array
-    {
-        return [new WhichBrowser()];
-    }
 
     /**
      * @param UserInterface $user
@@ -52,8 +41,6 @@ class Tracking extends AbstractService
         $connectionHistory->setTokenValue($token);
         $connectionHistory->setLoginDate(new \DateTime());
         $connectionHistory->setLastActionDate($connectionHistory->getLoginDate());
-
-        $this->updateConnectionFromUserAgent($connectionHistory);
 
         $this->PersistAndFlush($connectionHistory);
 
@@ -105,110 +92,5 @@ class Tracking extends AbstractService
         }
 
         return explode('.', $token)[1];
-    }
-
-    /**
-     * @param ConnectionHistory $connectionHistory
-     *
-     * @return ConnectionHistory
-     */
-    public function updateConnectionFromUserAgent(ConnectionHistory $connectionHistory): ConnectionHistory
-    {
-        $results = $this->parseUserAgent($connectionHistory->getUserAgent());
-
-        foreach ($results as $result) {
-            $this->updateBrowser($result, $connectionHistory);
-            $this->updateOperatingSystem($result, $connectionHistory);
-            $this->updateDevice($result, $connectionHistory);
-        }
-
-        return $connectionHistory;
-    }
-
-    /**
-     * @param string|null $userAgent
-     *
-     * @return array
-     */
-    protected function parseUserAgent(?string $userAgent): array
-    {
-        $results = [];
-
-        if ($userAgent) {
-            $providers = $this->getProviders();
-            foreach ($providers as $provider) {
-                try {
-                    $results[] = $provider->parse($userAgent);
-                } catch (NoResultFoundException $ex) {
-                    // nothing found
-                }
-            }
-        }
-
-        return $results;
-    }
-
-    /**
-     * @param UserAgent $result
-     * @param ConnectionHistory $connectionHistory
-     *
-     * @return ConnectionHistory
-     */
-    protected function updateBrowser(UserAgent $result, ConnectionHistory $connectionHistory): ConnectionHistory
-    {
-        $browser = $result->getBrowser();
-        $browserEngine = $result->getRenderingEngine();
-
-        if (empty($connectionHistory->getBrowserName())) {
-            $connectionHistory->setBrowserName($browser->getName());
-        }
-        if (empty($connectionHistory->getBrowserVersion())) {
-            $connectionHistory->setBrowserVersion($browser->getVersion()->getComplete());
-        }
-        if (empty($connectionHistory->getBrowserEngineName())) {
-            $connectionHistory->setBrowserEngineName($browserEngine->getName());
-        }
-        if (empty($connectionHistory->getBrowserEngineVersion())) {
-            $connectionHistory->setBrowserEngineVersion($browserEngine->getVersion()->getComplete());
-        }
-
-        return $connectionHistory;
-    }
-
-    /**
-     * @param UserAgent         $result
-     * @param ConnectionHistory $connectionHistory
-     */
-    protected function updateOperatingSystem(UserAgent $result, ConnectionHistory $connectionHistory)
-    {
-        if (empty($connectionHistory->getOperatingSystemName())) {
-            $connectionHistory->setOperatingSystemName($result->getOperatingSystem()->getName());
-        }
-        if (empty($connectionHistory->getOperatingSystemVersion())) {
-            $connectionHistory->setOperatingSystemVersion($result->getOperatingSystem()->getVersion()->getComplete());
-        }
-    }
-
-    /**
-     * @param UserAgent         $result
-     * @param ConnectionHistory $connectionHistory
-     */
-    protected function updateDevice(UserAgent $result, ConnectionHistory $connectionHistory)
-    {
-        if (empty($connectionHistory->getDeviceModel())) {
-            $connectionHistory->setDeviceModel($result->getDevice()->getModel());
-        }
-        if (empty($connectionHistory->getDeviceBrand())) {
-            $connectionHistory->setDeviceBrand($result->getDevice()->getBrand());
-        }
-        if (empty($connectionHistory->getDeviceType())) {
-            $connectionHistory->setDeviceType($result->getDevice()->getType());
-        }
-        if (empty($connectionHistory->isMobile())) {
-            $connectionHistory->setIsMobile($result->getDevice()->getIsMobile() === true);
-        }
-        if (empty($connectionHistory->isTouch())) {
-            $connectionHistory->setIsTouch($result->getDevice()->getIsTouch() === true);
-        }
     }
 }
