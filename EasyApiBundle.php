2a010f07004248c09518972a94da50bcd213a47e
citizen63000 <citizen.lantien@gmail.com>
2022-10-19 13:40:42 +0200

    new nelmio api-doc 4 & self constant support in doctrine annotation : maj controllers traits + remove unused nelmio overrides

--- EasyApiBundle.php
+++ EasyApiBundle.php
@@ -2,23 +2,8 @@
 
 namespace EasyApiBundle;
 
-use EasyApiBundle\DependencyInjection\Compiler\ConfigurationPass;
-//use EasyApiBundle\DependencyInjection\EasyApiExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class EasyApiBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        //        $container->registerExtension(new EasyApiExtension());
-
-        $container->addCompilerPass(new ConfigurationPass());
-
-    }
 }
